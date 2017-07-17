b8034c1dca9398730857ae3ce69de7ebd519d13e
Timothy Barnard <test>
2017-07-17 18:00:09 +0100

    updated

---
+++ githubIonic/src/enviornments/eviornment.prod.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.ts
+// used when we run `ng serve` or `ng build`
+export const environment = {
+  production: false,
+
+  // URL of development API
+  apiUrl: 'http://localhost:3000',
+  githubApiUrl: 'https://api.github.com'
+};
--- githubIonic/src/providers/github-users.ts
+++ githubIonic/src/providers/github-users.ts
@@ -4,29 +4,30 @@ import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 import { User } from '../models/user';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class GithubUsers {
-  githubApiUrl = 'https://api.github.com';
-  private readonly tokenURL: string = '&access_token=';
+  const GITHUB_API_URL = environment.githubApiUrl;
+  private readonly tokenURL: string = '&access_token=96e483489990723dce4ca7f8e6c307def8196a13';
 
   constructor(public http: Http) { }
 
   // Load all github users
   load(): Observable<User[]> {
-    return this.http.get(`${this.githubApiUrl}/users`)
+    return this.http.get(`${this.GITHUB_API_URL}/users`)
       .map(res => <User[]>res.json());
   }
 
    // Get github user by providing login(username)
   loadDetails(login: string): Observable<User> {
-    return this.http.get(`${this.githubApiUrl}/users/${login}`)
+    return this.http.get(`${this.GITHUB_API_URL}/users/${login}`)
       .map(res => <User>(res.json()))
   }
 
   // Search for github users  
   searchUsers(searchParam: string): Observable<User[]> {
-    return this.http.get(`${this.githubApiUrl}/search/users?q=${searchParam}${this.tokenURL}`) 
+    return this.http.get(`${this.GITHUB_API_URL}/search/users?q=${searchParam}${this.tokenURL}`) 
       .map(res => <User[]>(res.json().items))
   }
 
